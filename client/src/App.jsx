759f6d56ff4e604db0442230ada28988d5b32b53
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2023-11-18 15:51:56 +0200

    add react router dom
    add links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import Header from "../components/header/Header"
 import Home from "../components/home/Home"
+import GamesList from "../components/games-list/GamesList"
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   
@@ -7,7 +9,10 @@ function App() {
   return (
     <div id='box'>
       <Header />
-      <Home />
+      <Routes>
+        <Route path='/' element={ <Home /> } />
+        <Route path='/games' element={ <GamesList /> } />
+      </Routes>
     </div>
   )
 }
